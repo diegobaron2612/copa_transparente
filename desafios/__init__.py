38a12926109365c019d8afe7a2d5dba5117c997f
Vinicius Melo <viniciusdesk@icloud.com>
2020-02-17 20:54:43 -0300

    Adiciona funções ao init do desafio

--- desafios/__init__.py
+++ desafios/__init__.py
@@ -2,9 +2,11 @@
 
 from .aniversarios import retornar_aniversario
 from .calculadora import dividir
+from .duplicados import duplicados
 
 
 __all__ = (
-    'retornar_aniversario',
-    'dividir',
+    "retornar_aniversario",
+    "dividir",
+    "duplicados",
 )
